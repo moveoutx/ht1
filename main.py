2debdb03c195790b607862f5b5749392785077c6
Mikhail Ivanov <moveout@yandex.ru>
2022-09-25 20:11:24 +0300

    Задачи 1-4

---
+++ main.py
@@ -0,0 +1,10 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import datetime
+
+
+if __name__ == '__main__':
+    date = datetime.now()
+    print(date)
+    calculate_salary()
+    get_employees()
